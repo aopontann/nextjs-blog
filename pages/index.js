d97e2342a1d29771d7571910279e590ff5482e26
aopontann <aopontan0416@gmail.com>
2021-03-15 17:59:25 +0900

    test v0.0.6

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 //import { getSortedPostsData } from '../lib/posts'
 import { getFirestoreData } from '../lib/db'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { addBasePath } from 'next/dist/next-server/lib/router/router'
-
-/*const firebase = require("firebase");
-require("firebase/firestore");
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APPID,
-  measurementId: process.env.FIREBASE_MEASUREMENTID
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();*/
 
 /*export default function Home() {
   return (
@@ -54,16 +40,10 @@ export default function Home({ vtuberInfo, videoInfo }) {
               <a>name: {name}</a>
             </Link>
             <p>channelId: {channelId}</p>
+            <p>videos[0]: {videos[0]}</p>
           </div>
         ))}
-        {videoInfo.map(({id, title, thumbnails, URL}) => (
-          <div>
-            <img src={thumbnails}/>
-            <p>id: {id}</p>
-            <p>title: {title}</p>
-            <a href={URL}>クリック</a>
-          </div>
-        ))}
+        
       </ul>
       
     </Layout>
@@ -72,7 +52,23 @@ export default function Home({ vtuberInfo, videoInfo }) {
 
 export async function getStaticProps() {
   //const DBData = await getFirestoreData();
-  const vtuberInfo = [
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APPID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+  const DBData1 = await getFirestoreData(db, "ChroNoiR");
+  const DBData2 = await getFirestoreData(db, "えま★おうがすと");
+  const vtuberInfo = [DBData1, DBData2];
+  /*const vtuberInfo = [
     {
       name: "ChroNoiR",
       channelId: "UCz6vnIbgiqFT9xUcD6Bp65Q",
@@ -83,26 +79,11 @@ export async function getStaticProps() {
       channelId: "UCl1oLKcAq93p-pwKfDGhiYQ",
       videos: ["test3", "test4"]
     }
-  ];
-  const videoInfo = [
-    {
-      id: "OLWqLMbq5QY",
-      title: "MapleDancer【樋口楓オリジナル曲】",
-      thumbnails: "https://i.ytimg.com/vi/OLWqLMbq5QY/default.jpg",
-      URL: "https://www.youtube.com/watch?v=OLWqLMbq5QY"
-    },
-    {
-      id: "OLWqLMbq5QY",
-      title: "MapleDancer【樋口楓オリジナル曲】",
-      thumbnails: "https://i.ytimg.com/vi/OLWqLMbq5QY/default.jpg",
-      URL: "https://www.youtube.com/watch?v=OLWqLMbq5QY"
-    }
-  ];
+  ];*/
 
   return {
     props: {
-      vtuberInfo: vtuberInfo,
-      videoInfo: videoInfo
+      vtuberInfo: vtuberInfo
     }
   }
 }
